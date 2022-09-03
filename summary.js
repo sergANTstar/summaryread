b094368575bbb5f77656ebcbbfec41698b1d4832
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-09-03 19:35:56 +0300

    u

---
+++ summary.js
@@ -0,0 +1,9 @@
+const x = "https://github.com/sergANTstar/summaryread/blob/main/summary.md"
+
+const java = "https:"
+const script = "//github.com"
+const summary = "/sergANTstar/summaryread/blob/main/summary.md"
+
+const javaScript = java + script + summary
+
+console.log(javaScript)
